ff2092611b87a8aaa47ffe44d57796d37f60abcb
angelomelo <angelorobsonmelo@gmail.com>
2024-09-04 07:08:50 -0300

    implement navigation on both platform

---
+++ iosApp/iosApp/Screens/AboutScreen.swift
@@ -0,0 +1,35 @@
+//
+//  AboutScreen.swift
+//  iosApp
+//
+//  Created by Ângelo Melo on 03/09/24.
+//  Copyright © 2024 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct AboutScreen: View {
+    @Environment(\.dismiss)
+    private var dismiss
+    
+    var body: some View {
+        NavigationStack {
+            AboutListView()
+                .navigationTitle("About Device")
+                .toolbar {
+                    ToolbarItem(placement: .primaryAction) {
+                        Button {
+                            dismiss()
+                        } label: {
+                            Text("Done")
+                                .bold()
+                        }
+                    }
+                }
+        }
+    }
+}
+
+#Preview {
+    AboutScreen()
+}
